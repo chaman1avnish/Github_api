90cf4156dcdbcf4b358577e2cebb0b54f48404c8
chaman1avnish <avkumar1003@gmail.com>
2017-09-14 13:28:26 +0530

    create python api

---
+++ qblog/blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from . import models
+class EntryAdmin(admin.ModelAdmin):
+	list_display=("title","created")
+	prepopulated_fields={"slug"}
+
+admin.site.register(models.Entry)
+# Register your models here.
